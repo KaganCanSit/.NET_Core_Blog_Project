c0e4d1cdf4088502c6bbd1dd0ee9a6045aa19718
Kağan Can Şit <kagancansit@hotmail.com>
2022-06-27 21:56:42 +0300

    DatabaseBasicAdd
    Migration Connect Build and Core Packages Add Project

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Bağlantı adresinin tanıtılması - Bağlantı Class'ının tanımlanması
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //SQL Adres İsmi (Bağlantı Adresi) - Oluşturulacak Database İsmi 
+            optionsBuilder.UseSqlServer(@"Server=KAGANCANSIT; Database=CoreBlogDB; Integrated Security=True; Trusted_Connection=True;");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }    
+        public DbSet<Category> Categories { get; set; }    
+        public DbSet<Comment> Comments { get; set; }    
+        public DbSet<Contact> Contacts { get; set; }    
+        public DbSet<Writer> Writers { get; set; }    
+    }
+}
