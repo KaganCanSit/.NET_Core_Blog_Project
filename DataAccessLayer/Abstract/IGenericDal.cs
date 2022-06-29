a44c0c63dd9e86ba98cf1930465b9674f5d17873
Kağan Can Şit <kagancansit@hotmail.com>
2022-06-30 00:21:38 +0300

    30Jun

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    // Tüm nesneler içni tek tek Interface tanımlamaktansa bir class atamasıyla yanı methodları oluşturarak, ortak kullanabilirim. (DRY ilkesi)
+    // T Class parametresi var olan 
+    public  interface IGenericDal<T> where T:class 
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+
+        //Dışarında parametre almadan tüm veriyi getirecek fonksiyon.
+        List<T> GetListAll();
+        
+        //ID değerine göre içerik getirmek için method.
+        T GetById(int id);
+    }
+}
