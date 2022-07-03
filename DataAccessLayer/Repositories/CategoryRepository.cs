3144a6c335df98f82efff30665bbbf5612367f95
Kağan Can Şit <kagancansit@hotmail.com>
2022-07-03 15:42:19 +0300

    ThemeAdd_24LessonEnd

--- DataAccessLayer/Repositories/CategoryRepository.cs
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -27,6 +27,11 @@ namespace DataAccessLayer.Repositories
             c.SaveChanges(); //Değişikliklerin kayıt edilmesi için gerekli
         }
 
+        public void Delete(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
         public void DeleteCategory(Category category)
         {
             c.Remove(category);
@@ -38,9 +43,24 @@ namespace DataAccessLayer.Repositories
             return c.Categories.Find(id);
         }
 
+        public List<Category> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Category> ListAllCategory()
         {
             return c.Categories.ToList();
+        }
+
+        public void Update(Category t)
+        {
+            throw new NotImplementedException();
         }
 
         public void UpdateCategory(Category category)
