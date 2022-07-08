d4e9bf41b6b03ba660cc2145ee23ce55dd17fedd
Kağan Can Şit <kagancansit@hotmail.com>
2022-07-08 23:39:12 +0300

    Lesson_34

---
+++ BlogProject/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+    // Ekleme işlemi yapılırkem HttpGet ve HttpPost attributlerinin tanımlandığı metotların isimleri aynı olmak zorundadır.
+    // HttpGet -> Sayfa Yüklenince / HttpPost -> Sayfa Buton Tetiklenince (Parametre alması gerekir.)
+
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
