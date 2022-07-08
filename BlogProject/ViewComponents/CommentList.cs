aa6cd4c25bbcead93de6ffc92e6d97d1448789fa
Kağan Can Şit <kagancansit@hotmail.com>
2022-07-08 19:50:18 +0300

    Lesson_32

---
+++ BlogProject/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using BlogProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    Username = "Kağan"
+                },
+                new UserComment
+                {
+                    ID = 2,
+                    Username = "Mesut"
+                },
+                new UserComment
+                {
+                    ID = 3,
+                    Username = "Merve"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
