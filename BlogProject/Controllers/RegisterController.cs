3cd37ac4244175c3bf483634d3e281edcc5a33bc
Kağan Can Şit <kagancansit@hotmail.com>
2022-07-09 14:13:28 +0300

    Lesson_34

--- BlogProject/Controllers/RegisterController.cs
+++ BlogProject/Controllers/RegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.Controllers
 {
@@ -7,6 +10,9 @@ namespace BlogProject.Controllers
 
     public class RegisterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -14,9 +20,12 @@ namespace BlogProject.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Writer p)
         {
-            return View();
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Test";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index", "Blog");
         }
     }
 }
