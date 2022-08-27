bc9b64f4f6f1bccfd2d531573216912992cdf7ac
Kağan Can Şit <kagancansit@hotmail.com>
2022-08-27 17:18:36 +0300

    Lesson39_Done
    Mail and Comment Add

--- BlogProject/Controllers/CommentController.cs
+++ BlogProject/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.Controllers
@@ -13,8 +14,19 @@ namespace BlogProject.Controllers
             return View();
         }
 
+        [HttpGet]
         public PartialViewResult PartialAddComment()
         {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult PartialAddComment(Comment p)
+        {
+            p.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            p.BlogID = 10;
+            cm.CommentAdd(p);
             return PartialView();
         }
 
