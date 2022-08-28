7af8b0bda5406d574d6e1d1bd666444f7567b6a3
Kağan Can Şit <kagancansit@hotmail.com>
2022-08-28 16:05:05 +0300

    Lesson44_Done

---
+++ BlogProject/Controllers/WriterController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+    //Kullanici yetkisi bulunmadan goruntulenmemesini sagliyor.
+    [Authorize]
+
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+    }
+}
