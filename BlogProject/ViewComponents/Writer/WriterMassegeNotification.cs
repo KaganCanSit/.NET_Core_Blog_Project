cd7f4494aa371b56b4693d7c382e61955efbeba0
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-04 13:56:32 +0300

    Lesson52_Done

---
+++ BlogProject/ViewComponents/Writer/WriterMassegeNotification.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents.Writer
+{
+    public class WriterMassegeNotification : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { 
+           return View();
+        }
+    }
+}
