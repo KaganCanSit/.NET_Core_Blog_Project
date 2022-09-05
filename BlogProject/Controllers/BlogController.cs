a42dbc8a97b84800e736ab92860d541e9e1b08d6
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-05 23:09:42 +0300

    Lesson59_Done

--- BlogProject/Controllers/BlogController.cs
+++ BlogProject/Controllers/BlogController.cs
@@ -80,5 +80,18 @@ namespace BlogProject.Controllers
             bm.TDelete(blogvalue);
             return RedirectToAction("BlogListByWriter");
         }
+
+        [HttpGet]
+        public IActionResult EditBlog(int id)
+        {
+            var blogvalue = bm.TGetById(id);
+            return View(blogvalue);
+        }
+
+        [HttpPost]
+        public IActionResult EditBlog(Blog p)
+        {
+            return RedirectToAction("BlogListByWriter");
+        }
     }
 }
