30ee8edce38e3020e22f910e76742923e67042c8
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-10 15:45:05 +0300

    Lesson72_Done

--- BlogProject/Controllers/WriterController.cs
+++ BlogProject/Controllers/WriterController.cs
@@ -1,7 +1,11 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.DependencyResolver;
 
 namespace BlogProject.Controllers
 {
@@ -44,10 +48,32 @@ namespace BlogProject.Controllers
         }
 
         [AllowAnonymous]
+        [HttpGet]
         public IActionResult WriterEditProfile()
         {
             var writervalues = wm.TGetById(1);
             return View(writervalues);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult results = wl.Validate(p);
+            if(results.IsValid)
+            {
+                wm.TUpdate(p);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach(var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
