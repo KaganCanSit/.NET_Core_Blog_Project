f8b8383443505cc361a44bc8832ee2fab97e00fe
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-11 19:41:50 +0300

    Lesson84_Done

---
+++ BlogProject/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlogProject.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        
+        //Mesajların Tamamı
+        public IActionResult InBox()
+        {
+            int id = 1;
+            var values = mm.GetInboxListByWriter(id);
+            return View(values);
+        }
+
+
+        public IActionResult MessageDetails(int id)
+        {
+            //Kategori ID yerine kategori isimlerini yazdırılması
+            var value = mm.TGetById(id);
+            return View(value);
+        }
+    }
+}
