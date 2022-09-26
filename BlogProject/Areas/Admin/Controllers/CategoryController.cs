add08ccefa4b851efed3041c878759bec21e7cf2
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-26 17:17:57 +0300

    Lesson93_Done

--- BlogProject/Areas/Admin/Controllers/CategoryController.cs
+++ BlogProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 //Listeleme islemi icin ekledigimiz Nuget paketi
 using X.PagedList;
@@ -10,11 +13,41 @@ namespace BlogProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index(int page=1)
+        public IActionResult Index(int page = 1)
         {
             //Sayfalamanin baslayacagi ve sayfada bulunan deger sayisi
-            var values = cm.GetList().ToPagedList(page,5);
+            var values = cm.GetList().ToPagedList(page, 5);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            //Kontrol için oluşturduğumuz class ile bir nesne oluşturuyoruz. Oluşturduğumuz nesne aracılığyla Writer parametresini göndererek kontrol sağlıyoruz.
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                //Hata varsa hatanın gerçekleştiği özelliğin ismi ve hata değerini dön.
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
