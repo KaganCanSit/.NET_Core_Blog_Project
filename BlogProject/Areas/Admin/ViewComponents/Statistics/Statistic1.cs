e5449324701e34ed0f04bed59b0ad81b33629817
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-27 18:20:23 +0300

    Lesson99_Done

---
+++ BlogProject/Areas/Admin/ViewComponents/Statistics/Statistic1.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Areas.Admin.ViewComponents.Statistics
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            //Blog sayisini getiriyoruz.
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            return View();
+        }
+    }
+}
