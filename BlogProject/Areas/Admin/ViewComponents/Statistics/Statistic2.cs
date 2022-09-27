1127e573c80da6547e509896304a7f07d7d50b0a
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-27 18:51:10 +0300

    Lesson100_Done

---
+++ BlogProject/Areas/Admin/ViewComponents/Statistics/Statistic2.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Areas.Admin.ViewComponents.Statistics
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            //Bloglari sirala en daha sonra listenin sonundakini al.
+            ViewBag.v1 = c.Blogs.OrderByDescending(X=>X.BlogID).Select(x=>x.BlogTitle).Take(1).FirstOrDefault();
+            ViewBag.v2 = c.Comments.Count();
+            return View();
+        }
+    }
+}
