cedfb3a6e6298cda6e7f92932f04c5b60cb6b7b2
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-27 11:53:26 +0300

    Lesson95_Done

---
+++ BlogProject/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,47 @@
+﻿using BlogProject.Areas.Admin.Models;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Areas.Admin.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            //Excel Icerigi Olusturdugumuzu Dusun
+            using (var workbook= new XLWorkbook())
+            {
+                //Excel Sayfasinin Ismi
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2; //Ilk satir zaten baslik
+                foreach(var item in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+
+                using(var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.opexmlformats - officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+                new BlogModel{ID=1, BlogName="C# Programlamaya Giriş"},
+                new BlogModel{ID=2, BlogName="Tesla Firmasının Araçları"},
+                new BlogModel{ID=3, BlogName="2020 Olimpiyatları"}
+            };
+            return bm;
+        }
+    }
+}
