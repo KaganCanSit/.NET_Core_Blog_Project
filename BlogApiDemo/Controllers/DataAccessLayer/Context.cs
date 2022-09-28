3c422e44946caf6c8de42d5c8ad69a0f05459cae
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-29 00:31:57 +0300

    Lesson114_Done

---
+++ BlogApiDemo/Controllers/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.Controllers.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //SQL Adres İsmi (Bağlantı Adresi) - Oluşturulacak Database İsmi 
+            optionsBuilder.UseSqlServer(@"Server=KAGANCANSIT; Database=CoreBlogApiDB; Integrated Security=True; Trusted_Connection=True;");
+        }
+        public DbSet<Employee> Employees  { get; set; }
+    }
+}
