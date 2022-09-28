a2824ee18d2c882672085ec15a5f051825ea7727
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-28 14:55:40 +0300

    Lesson103-104Done

---
+++ BlogProject/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,34 @@
+﻿using BlogProject.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass { 
+                CategoryName="Teknoloji", 
+                CategoryCount=10
+            });
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                CategoryName = "Spor",
+                CategoryCount = 5
+            });
+            return Json(new {jsonList = list}); 
+        }
+    }
+}
--- BlogProject/Areas/Admin/ViewComponents/Statistics/Statistic4.cs
+++ BlogProject/Areas/Admin/ViewComponents/Statistics/Statistic4.cs
@@ -8,9 +8,9 @@ namespace BlogProject.Areas.Admin.ViewComponents.Statistics
         Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.Name).FirstOrDefault();
-            ViewBag.v2 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.ImageURL).FirstOrDefault();
-            ViewBag.v3 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.ShortDescription).FirstOrDefault();
+            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 2).Select(y => y.Name).FirstOrDefault();
+            ViewBag.v2 = c.Admins.Where(x => x.AdminID == 2).Select(y => y.ImageURL).FirstOrDefault();
+            ViewBag.v3 = c.Admins.Where(x => x.AdminID == 2).Select(y => y.ShortDescription).FirstOrDefault();
             return View();
         }
     }
