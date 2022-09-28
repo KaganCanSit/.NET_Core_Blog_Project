a3180dfc344ec7610a0ec72ce04537cd451a6d28
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-29 00:40:37 +0300

    Lesson 115 Done

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using BlogApiDemo.Controllers.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+//API CONTROLLER
+namespace BlogApiDemo.Controllers
+{
+    //API'yi bir web projesi icerisinde cagirabilmek icin olmasi gereken tanim
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var value = c.Employees.ToList();
+            return Ok(value);
+        }
+    }
+}
