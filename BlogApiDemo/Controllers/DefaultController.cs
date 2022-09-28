5218fe2679c8430ec8681f839c4175cb19fd1a86
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-29 01:03:14 +0300

    Lesson 177 - 119 Done | Basic API Operation Simple

--- BlogApiDemo/Controllers/DefaultController.cs
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -18,10 +18,67 @@ namespace BlogApiDemo.Controllers
             return Ok(value);
         }
 
+        //Yeni employee eklenmesi islemi - Add
         [HttpPost]
-        public IActionResult EmployeeAdd()
+        public IActionResult EmployeeAdd(Employee employee)
         {
+            using var c = new Context();
+            c.Add(employee);
+            c.SaveChanges();
             return Ok();
+        }
+
+        //Employee bilgisini Get üzerinden ip ile getir. Bulmak icin ise Employee icerisinde ara/find et.
+        [HttpGet("{id}")]
+        public IActionResult EmployeeGet(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+        }
+
+        //Employee bilgisini ip ile bularak silme islemi - Delete
+        [HttpDelete("{id}")]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Employees.Remove(employee);
+                c.SaveChanges();
+                return Ok();
+            }
+        }
+
+        //Employee bilgisini Put yardimiyla guncelleme islemi - Delete
+        [HttpPut]
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new Context();
+            var emp = c.Find<Employee>(employee.ID);//Employee Bul
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.Name = employee.Name;
+                c.Update(emp);
+                c.SaveChanges();
+                return Ok();
+            }
         }
     }
 }
