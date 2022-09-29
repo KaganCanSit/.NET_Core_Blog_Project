b5d2d4cf94abe688a7aa6baccca51accbb142849
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-29 15:14:53 +0300

    Lesson 125 Done | Identity Login

--- BlogProject/Controllers/LoginController.cs
+++ BlogProject/Controllers/LoginController.cs
@@ -1,59 +1,46 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BlogProject.Models;
+using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace BlogProject.Controllers
 {
+    [AllowAnonymous] //Authentication Islemlerinden Muaf Ol!
     public class LoginController : Controller
     {
-        //Authentication Islemlerinden Muaf Ol!
-        [AllowAnonymous]
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
-
             return View();
         }
 
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Index(Writer p)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            //Clamins Prensiple = Talepler
-            Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if(datavalue!= null)
+            if(ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                //Kullanıcı  adı - sifre - cerezlerde hatirlasin - yanlis girme durumu(engel)
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if(result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name , p.WriterMail)
-                };
-                //Neden burada ikinci bir string degeri gonderilmesi gerekiyor?
-                var useridentiy = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentiy);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
             }
-            else
-            {
-                return View();
-            }
-
-            /*
-            Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if(datavalue != null)
-            {
-                HttpContext.Session.SetString("username", p.WriterMail);
-                return RedirectToAction("Index", "Writer");
-            }
-            else
-            {
-                return View();
-            }
-            */
+            return View();
         }
     }
 }
