a87359999753da2840803afadeca43e6657293c2
Kağan Can Şit <kagancansit@hotmail.com>
2022-09-29 16:47:57 +0300

    Lesson 127 Done

--- BlogProject/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ BlogProject/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.ViewComponents.Writer
@@ -9,10 +11,12 @@ namespace BlogProject.ViewComponents.Writer
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
         Context c = new Context();
-
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var username = User.Identity.Name;
+            ViewBag.mail = username;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(X => X.WriterMail == usermail).Select(y=>y.WriterID).FirstOrDefault();
             var values = wm.GetBWriterById(writerID);
             return View(values);
